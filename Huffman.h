38bcef032c547b573216f870523e958a9944db10
Prathamesh <devpsk03@gmail.com>
2024-04-06 23:44:05 +0530

    Completed Huffman coding main code header

---
+++ Huffman.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <queue>
+#include <fstream>
+
+#include "Node.h"
+
+class Huffman
+{
+private:
+  std::unordered_map<char, std::string> codeMap;
+  void encodeCharacters(Node *rootNode, std::string codeString);
+  void writeHeader(std::ofstream &outputStream);
+  void readHeader(std::ifstream &inputStream);
+  Node *buildDecodingTree(std::unordered_map<char, std::string> encodingMap);
+
+public:
+  void huffer(std::unordered_map<char, int> frequencyMap);
+  void dehuffer(std::string fileName, std::string decompressedFileName);
+  void compressToFile(std::string inputFileName, std::string outputFileName);
+  void decompressToFile(Node *rootNode, std::string codeString, std::string decompressFileName);
+};
+
+class MyComparator
+{
+public:
+  bool operator()(Node *node1, Node *node2);
+};
